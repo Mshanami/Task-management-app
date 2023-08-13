e40322b521cc848a2ace01ba07aaa2e1ed67e51f
Mshanami <115123223+Mshanami@users.noreply.github.com>
2023-08-13 12:34:28 +0000

    improving over learned concepts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { useState } from 'react';
+import 'animate.css';
 import {BsTelephone} from 'react-icons/bs'
 
 function App() {
-  const [sports,setSports] = useState([]);
-  const [spoinput, setSpoinput]=useState('');
+  const [tasks,setTasks] = useState([]);
+  const [taskinput, setTaskinput]=useState('');
+  const [emptyInput, setEmptyInput]=useState(true);
+
+  const today = new Date();
+  const dayOfWeek = today.toLocaleString('default', {weekday : 'long'});
+  const date = today.toLocaleString('default',{ day: 'numeric', month: 'long'});
+
   const handleChange = (event) =>{
-    setSpoinput(event.target.value);
+    const value=event.target.value
+    setTaskinput(value);
+    setEmptyInput(value === '')
     
   };
   
   const handleSubmit = (event) =>{
     event.preventDefault();
-    setSports([...sports, spoinput]);
-    setSpoinput('')
-    console.log(spoinput);
+    setTasks([...tasks, taskinput]);
+    setTaskinput('')
+    console.log(taskinput);
   }
+  const handleDone = (index) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      updatedTasks[index] = <span style={{ textDecoration: 'line-through' }}>{prevTasks[index]}</span>;
+      return updatedTasks;
+    });
+  };
   const handleDelete = (index) =>{
-    const updatedSports=[...sports];
-    updatedSports.splice(index, 1);
-    setSports(updatedSports);
+    const updatedTasks=[...tasks];
+    updatedTasks.splice(index, tasks.length);
+    setTasks(updatedTasks);
 
     console.log('Deleted')
   
@@ -28,27 +44,31 @@ function App() {
 
   return (
     <div className='App'>
-    <h2>What to do today?</h2>
+    <h2>Easy Way To Manage Your Daily Tasks.</h2>
+    
    <div className='task'>
+    <p className='date'>{dayOfWeek} {date}</p>
     <form onSubmit={handleSubmit}>
       <input type='text'
-       value={spoinput}
+       value={taskinput}
        onChange={handleChange}
        placeholder='add your task here....'
        required
         />
-       <button type="submit">Add task</button>
+        <button type="submit" disabled={emptyInput} className={emptyInput? "disabled-button":""} >Add task</button>
 
         </form>
         
         <ul>
-      {sports.map((sport,index) => <li key={index}>{sport}
-      
+      {tasks.map((task,index) => <li key={index}>{task}
+      <button className='done' onClick={() => handleDone(index)}>Done</button>
       </li>)}
-      <button className='remove' onClick={handleDelete}>Clear tasks</button>
+      {tasks.length > 0 ?<button className='remove'  onClick={handleDelete}>Clear tasks</button> : <div></div>}
+      
       </ul>
       
     </div>
+    <h2 className='animate__animated animate__fadeOutDown'>Wishing you a productive day</h2>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5JvaTSDU4u4KZVUdRWF8p-zuxt1OwP_0",
+  authDomain: "task-management-6865e.firebaseapp.com",
+  projectId: "task-management-6865e",
+  storageBucket: "task-management-6865e.appspot.com",
+  messagingSenderId: "527412303397",
+  appId: "1:527412303397:web:912635d6f0381c9466d1cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
